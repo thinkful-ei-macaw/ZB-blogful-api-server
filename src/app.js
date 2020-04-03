bdec3935a6317fcac53898c0e75011c004efe728
Brannen Petit <bpetit940@gmail.com>
2020-04-03 11:30:42 -0500

    refactoring and added testing for post endpoint

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
+const articlesRouter = require("./articles/articles-router");
+
 const { NODE_ENV } = require("./config");
 
 const app = express();
@@ -12,6 +14,8 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use("/articles", articlesRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
